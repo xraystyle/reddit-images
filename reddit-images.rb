699cf1bb2445e41f4603992cd879e850f7038655
xraystyle <xraystyle@xraystyle.com>
2015-06-26 12:51:44 -0700

    Create a main layout, move the head and title code to it and remove it from the other templates.

--- reddit-images.rb
+++ reddit-images.rb
@@ -1,15 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'tilt/haml'
 
 
 get '/' do
-    haml :index, format: :html5
+    haml :index, format: :html5, layout: :main_layout
 end
 
 
 post '/images/' do
-    sub = params[:subreddit]
-    score = params[:score]
-    sort = params[:sort]
-    "params were: #{sub}, #{score}, #{sort}."
+    # sub = params[:subreddit]
+    # score = params[:score]
+    # sort = params[:sort]
+    # "params were: #{sub}, #{score}, #{sort}."
+    haml :images, format: :html5, layout: :main_layout
 end
