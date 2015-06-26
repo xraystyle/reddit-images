00ddbbe3e14498b8ec7424ec556c667569a3851a
xraystyle <xraystyle@xraystyle.com>
2015-06-26 12:31:03 -0700

    Build the form, pass the correct params to the server.

--- reddit-images.rb
+++ reddit-images.rb
@@ -5,3 +5,11 @@ require 'sinatra/reloader'
 get '/' do
     haml :index, format: :html5
 end
+
+
+post '/images/' do
+    sub = params[:subreddit]
+    score = params[:score]
+    sort = params[:sort]
+    "params were: #{sub}, #{score}, #{sort}."
+end
