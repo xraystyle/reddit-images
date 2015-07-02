8244f1be9cf97da385aafd8a5b7b368b674a1adc
xraystyle <xraystyle@xraystyle.com>
2015-07-01 19:50:46 -0700

    add a catch-all route that redirects back to root.

--- reddit-images.rb
+++ reddit-images.rb
@@ -12,11 +12,14 @@ get '/' do
     haml :index, format: :html5, layout: :main_layout
 end
 
-
 post '/images/' do
     page_array = get_pages(params[:subreddit],  params[:sort], params[:howmany])
 
     @images = parse_pages(page_array, params[:score])
     
     haml :images, format: :html5, layout: :main_layout
+end
+
+get '/*' do
+    redirect to('/'), 303
 end
