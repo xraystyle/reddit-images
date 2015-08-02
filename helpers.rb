20b48ad5bea72c3717f0b5154aa432402dd17995
xraystyle <xraystyle@xraystyle.com>
2015-08-02 13:26:01 -0700

    Get scraper mostly working, with some CSS styling.

--- helpers.rb
+++ helpers.rb
@@ -46,7 +46,7 @@ module Helpers
         posts.each do |post|
             # process the URL to see if it's a useful image.
             # if so, add it to the image_urls array.
-            urls = get_post_urls(post)
+            urls = get_post_urls(post) # see the get_post_urls method below.
             image_urls << urls if urls
         end
         # return the array of image urls.
@@ -65,26 +65,31 @@ module Helpers
         url_pair = { post_url: "http://reddit.com" + post["data"]["permalink"], image_url: nil, format: nil }
 
         case url_value
-        when /https?:\/\/imgur.com\/a\/.*/  #imgur album, get the first image
+        #imgur album, get the cover image.
+        when /https?:\/\/imgur.com\/a\/(.*)/
            
-            # Edge cases occur where reddit doesn't pull the first image.
-            # When that happens, there's no thumbnail preview on the post, and the 
-            # oembed hash doesn't exist, causing a NoMethodError.
+            # Get the imgur id of the album.
+            album_id = $1
+            link_data = pull_imgur(album_id, :album)
 
-            begin
-                url_pair[:image_url] = post['data']['media']['oembed']['thumbnail_url']
-            end
+            url_pair[:image_url] = link_data[:link]
+            url_pair[:format] = link_data[:format]
 
-        when /https?:\/\/[a-zA-Z0-9\-._~\/]+\.(jpg|gifv|gif|png)/ #any direct URL to an image.
-            # Just return the value.
-            url_pair[:image_url] = url_value
-
-        when /https?:\/\/(m\.)?imgur.com\/[a-zA-Z0-9]+$/ #imgur single image on a page. Also handles mobile link posts.
+        #imgur single image on a page, or direct link. Also handles mobile link posts.
+        when /https?:\/\/(m|i\.)?imgur.com\/([a-zA-Z0-9]+)(\..*)?$/
             # adding the 'i.' and the '.jpg' to the url goes straight to the image,
             # regardless of format. (jpg, png, gif, etc.)
-            url_value.sub!(/m\./, "")
-            url_value.sub!(/imgur/, "i.imgur")
-            url_pair[:image_url] = url_value + ".jpg"
+            image_id = $2
+            link_data = pull_imgur(image_id, :image)
+
+            url_pair[:image_url] = link_data[:link]
+            url_pair[:format] = link_data[:format]
+
+        #any direct URL to an image that's not imgur.
+        when /https?:\/\/[a-zA-Z0-9\-._~\/]+\.(jpg|gif|png)/
+            # Just return the value.
+            url_pair[:image_url] = url_value
+            url_pair[:format] = :image
         end
 
         # ignore unmatched urls.
@@ -94,6 +99,47 @@ module Helpers
         
     end
 
+
+    # query the imgur API for useful data about the link, return usable info.
+    # Type can be either :image or :album
+    def pull_imgur(id, type)
+
+        # query imgur for json data on the object in question.
+        begin
+            raw = open(IMGUR_BASE + type.to_s + "/#{id}", "Authorization" => API_KEY).read
+        rescue
+            # returning these nils essentially skips the image in the output 
+            # if there's an error when querying the imgur API.
+            return { format: nil, link: nil }
+        end
+
+        json = JSON.parse(raw)
+
+        if type == :image
+
+            info = Hash.new # we return this at the end of the method.
+
+            # get the type and direct link.
+            format = json['data']['type']
+            info[:link] = json['data']['link']
+            
+            if format == "image/gif"
+                info[:format] = :gifv
+            else
+                info[:format] = :image
+            end
+
+        end
+
+        # If the id passed is for an album, we want the info for the cover image.
+        # Call the method recursively with the ID of the cover image.
+        if type == :album
+            cover_id = json['data']['cover']
+            info = pull_imgur(cover_id, :image) 
+        end
+
+        info
+    end
 
 
     def format_link(url_pair)
